5af8ea2a3d279c872cd6a218dcee6462a2915d24
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-24 16:21:13 -0300

    Criando a model

---
+++ lojaDeGames/src/main/java/com/minha/lojaDeGames/model/Categoria.java
@@ -0,0 +1,45 @@
+package com.minha.lojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotNull
+	@Size(min=2, max=100)
+	private String classificacao;
+	@NotNull
+	private boolean receita;
+	//@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	//@JsonIgnoreProperties("categoria")
+	//private List <Produto> produto;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getClassificacao() {
+		return classificacao;
+	}
+	public void setClassificacao(String classificacao) {
+		this.classificacao = classificacao;
+	}
+	public boolean isReceita() {
+		return receita;
+	}
+	public void setReceita(boolean receita) {
+		this.receita = receita;
+	}
+	
+}
